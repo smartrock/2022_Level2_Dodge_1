cf5c820f45c79a2c1a49d48936f69dfae83e8d19
smartrock <samorsler@mmc.school.nz>
2022-06-13 10:48:23 +1200

    This version has planets moving and the spaceship drawn

--- FrmDodge.cs
+++ FrmDodge.cs
@@ -16,6 +16,7 @@ namespace _2022_Level2_Dodge
         Graphics g; //declare a graphics object called g
         Planet[] planet = new Planet[7];// declare space for an array of 7 objects called planet 
         Random yspeed = new Random();
+        Spaceship spaceship = new Spaceship();
         public FrmDodge()
         {
             InitializeComponent();
@@ -40,6 +41,7 @@ namespace _2022_Level2_Dodge
                 //call the Planet class's drawPlanet method to draw the images
                 planet[i].DrawPlanet(g);
             }
+            spaceship.DrawSpaceship(g);
         }
 
         private void TmrPlanet_Tick(object sender, EventArgs e)
---
+++ Spaceship.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace _2022_Level2_Dodge
+{
+    class Spaceship
+    {
+        // declare fields to use in the class
+
+        public int x, y, width, height;//variables for the rectangle
+        public Image spaceship;//variable for the planet's image
+
+        public Rectangle spaceRec;//variable for a rectangle to place our image in
+
+        //Create a constructor (initialises the values of the fields)
+        public Spaceship()
+        {
+            x = 10;
+            y = 360;
+            width = 40;
+            height = 40;
+            spaceship = Properties.Resources.alien1;
+            spaceRec = new Rectangle(x, y, width, height);
+        }
+        //methods
+        public void DrawSpaceship(Graphics g)
+        {
+
+            g.DrawImage(spaceship, spaceRec);
+        }
+    }
+}
